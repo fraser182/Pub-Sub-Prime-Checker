c603ecde1b18eb1aee858278d9b0e8d46a60faf0
Fraser Ross <fraser182@icloud.com>
2019-01-07 20:52:52 +0000

    homework complete - if statement for true/false change

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -8,10 +8,24 @@ ResultView.prototype.bindEvents = function () {
   PubSub.subscribe('PrimeNumber:result-calculated', (event) => {
     const isItPrime = event.detail;
     console.log(`payload received in ResultView`, isItPrime);
-    
+
+    this.displayResult(isItPrime);
 
   })
 
-};
+}
+
+
+ResultView.prototype.displayResult = function(result){
+
+  const resultElement = document.querySelector('#result')
+  if (result === true) {
+    resultElement.textContent = `This is a Prime Number!`;
+  }
+    else {
+      resultElement.textContent = `This is NOT a Prime Number`;
+  }
+
+}
 
 module.exports = ResultView;
