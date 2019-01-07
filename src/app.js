349c9c6c6dc72b106abbcd77be7a0cd5dec200ab
Fraser Ross <fraser182@icloud.com>
2019-01-07 16:38:09 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const FormView = require('./views/form_view.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+
+const formView = new FormView();
+formView.bindEvents();
+
+
+
+
+});
---
+++ src/views/form_view.js
@@ -0,0 +1,19 @@
+const PubSub = require ('../helpers/pub_sub.js')
+
+const FormView = function (){
+};
+
+FormView.prototype.bindEvents = function(){
+  const form = document.querySelector('#prime-checker-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const inputtedNumber = event.target.number.value;
+    console.log('inputted number', inputtedNumber);;
+
+  });
+
+
+}
+
+module.exports = FormView;
